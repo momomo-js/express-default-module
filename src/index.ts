e95abb814c5846d7c3447492e8862bf3a5cef320
益生菌 <yskun@outlook.com>
2017-09-01 13:46:46 +0800

    变更 匹配@mojs/core插件方法

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,46 @@
-
-import {ExpressMiddleware} from "@mo/express";
-import {Plugin} from "@mo/core";
+import {ExpressMiddleware, ExpressServer} from '@mo/express';
 import * as e from 'express';
-import * as logger from "morgan";
-import * as bodyParser from "body-parser";
-import * as cookies from "cookie-parser";
-import * as session from "express-session";
-import * as CORS from "cors";
-import * as helmet from "helmet";
-import {SessionOptions} from "express-session";
-import {CorsOptions} from "cors";
-import {IHelmetConfiguration} from "helmet";
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as cookies from 'cookie-parser';
+import * as session from 'express-session';
+import * as CORS from 'cors';
+import * as helmet from 'helmet';
+import {SessionOptions} from 'express-session';
+import {CorsOptions} from 'cors';
+import {IHelmetConfiguration} from 'helmet';
+import {Plugin, PluginPackage} from '@mo/core';
 
-
-
+@PluginPackage(ExpressServer)
 export class ExpressDefaultPluginPackage {
+
     sessionOption: SessionOptions = {
         secret: 'NEXTION_DEFAULT_SESSION',
         cookie: {
-            maxAge: 60000 * 20	//20 minutes
+            maxAge: 60000 * 20	// 20 minutes
         },
 
         resave: true,
         saveUninitialized: true
     };
-
-    corsOption: CorsOptions = null;
-
-    helmetOption: IHelmetConfiguration = null;
-
-    @Plugin(ExpressMiddleware)
-    cors: Function = CORS(this.corsOption);
-
-    @Plugin(ExpressMiddleware)
-    helmet: Function = helmet(this.helmetOption);
-
-    @Plugin(ExpressMiddleware)
-    logger: Function = logger(process.env.NODE_ENV === 'production' ? 'common' : 'dev');
-
-
-    @Plugin(ExpressMiddleware)
-    bodyParser = bodyParser.json();
-
-    @Plugin(ExpressMiddleware)
-    bodyParserUrl = bodyParser.urlencoded({extended: false});
-
-    @Plugin(ExpressMiddleware)
-    cookies = cookies();
-
     @Plugin(ExpressMiddleware)
     session = session(this.sessionOption);
-
+    corsOption: CorsOptions = {};
+    @Plugin(ExpressMiddleware)
+    cors: Function = CORS(this.corsOption);
+    helmetOption: IHelmetConfiguration = {};
+    @Plugin(ExpressMiddleware)
+    helmet: Function = helmet(this.helmetOption);
+    @Plugin(ExpressMiddleware)
+    logger: Function = logger(process.env.NODE_ENV === 'production' ? 'common' : 'dev');
+    @Plugin(ExpressMiddleware)
+    bodyParser = bodyParser.json();
+    @Plugin(ExpressMiddleware)
+    bodyParserUrl = bodyParser.urlencoded({extended: false});
+    @Plugin(ExpressMiddleware)
+    cookies = cookies();
+    @Plugin(ExpressMiddleware)
+    corsRouter: Function = e.Router().options('*', CORS());
 }
 
 /**
--- test/index2.ts
+++ test/index2.ts
@@ -1,10 +1,117 @@
+import {Component, Controller, Instance, Method, Module, MoServer, Router, RouterManager} from '@mo/core';
+import {Injectable} from 'injection-js';
+import {ArrayType, Param, Query} from "@mo/express/src/decoration/parameter";
+import {Express, ExpressServer, GET, POST, ResponseHandler} from "@mo/express";
+import {ExpressDefaultPluginPackage} from "../src/index";
 
-import {MoServer} from "@mo/core";
-import {ExpressServer} from "@mo/express";
-import {ExpressDefaultPluginPackage} from "../index";
 
-let server = new MoServer();
-let express = new ExpressServer();
-server.addServer(express);
-express.addPlugin(new ExpressDefaultPluginPackage());
-server.startSever();
+class IndexModel {
+    test = Number;
+    haha = Number;
+}
+
+class NewIndexModel {
+    @Query
+    @ArrayType(Number)
+    test: number[];
+
+    @Param
+    ts: number;
+
+    @Query
+    yy: number;
+}
+
+
+@Controller({
+    models: [
+        IndexModel,
+        NewIndexModel
+    ],
+    path: '/'
+})
+class IndexController {
+
+    @Method(GET, '/')
+    @Express({
+        responds: [{
+            status: 1,
+            message: '完成响应'
+        }]
+    })
+    async index(model: IndexModel, res: ResponseHandler): Promise<ResponseHandler> {
+        res.status(1).body(model);
+        return res;
+    }
+
+    @Method(POST, '/:ts')
+    @Express({
+        responds: [{
+            status: 1,
+            message: '完成响应'
+        }]
+    })
+    post(model: NewIndexModel, res: ResponseHandler): ResponseHandler {
+        res.status(1).body(model);
+        return res;
+    }
+}
+
+@Router({
+    controllers: [
+        IndexController
+    ]
+})
+class IndexRouter {
+}
+
+@Injectable()
+class TestComponent extends Component {
+
+    constructor(public router: RouterManager) {
+        super();
+    }
+
+    onInit(): void {
+        this.debug(`testing onInit...`);
+        this.debug(`${this.router.constructor.name}`)
+    }
+
+    onStart(): void {
+        this.debug(`testing onStart...`);
+
+    }
+
+    onStop(): void {
+    }
+
+}
+
+@Module({
+    plugins: [],
+    components: [TestComponent]
+})
+class TestModule {
+}
+
+
+@Instance({
+    servers: [ExpressServer],
+    modules: [TestModule, TestModule],
+    routers: [IndexRouter],
+    components: [TestComponent, TestComponent],
+    plugins: [ExpressDefaultPluginPackage],
+    instance: {
+        name: 'TEST',
+        host: 'localhost',
+        port: 3000
+    }
+})
+class TestInstance {
+}
+
+new MoServer(TestInstance).startSever().then();
+
+/**
+ * Created by yskun on 2017/7/8.
+ */
